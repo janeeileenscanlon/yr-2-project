0393750bc3139fd36a1fb2ecd3e4d13291b4e818
niamhodowd <niamhemmaodowd@gmail.com>
2025-11-27 18:01:00 +0000

    Update db.php

--- db.php
+++ db.php
@@ -10,4 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+    $conn->set_charset("utf8");
+
 ?>
+
